2a26997834fa42740b7ff2f547a6e032b276eb3a
Connor Bartlett <connorbartlett@Connors-MacBook-Pro.local>
2019-03-07 14:18:49 -0500

    styled home page iteration 1: pre navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
@@ -16,4 +18,3 @@ import { AppComponent } from './app.component';
 export class AppModule { }
 
 
-//this is a test -connor
